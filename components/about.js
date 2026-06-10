2037f70545afebaa7463deadb43d8f9d615c5421
RahulKumar55 <111222140+RahulKumar55@users.noreply.github.com>
2026-06-09 21:04:14 -0400

    res upd

--- components/about.js
+++ components/about.js
@@ -48,10 +48,21 @@ const About = () => {
 
                 <div className="bg-greyCustom p-6 rounded-lg md:flex justify-between items-center">
                   <div className="mb-4 md:mb-0">
-                    <p className="text-lg">Programming Lab Assistant at Seneca College</p>
+                    <p className="text-lg">Teaching Assistant at Seneca College</p>
                   </div>
                   <div className="text-right md:text-right">
-                    <p className="text-lg">Sept 2024 - Apr 2024</p>
+                    <p className="text-lg">Sept 2024 - Aug 2024</p>
+                  </div>
+                </div>
+                
+                <br/>
+
+                <div className="bg-greyCustom p-6 rounded-lg md:flex justify-between items-center">
+                  <div className="mb-4 md:mb-0">
+                    <p className="text-lg">Machine Operator at Hood Packaging</p>
+                  </div>
+                  <div className="text-right md:text-right">
+                    <p className="text-lg">Jan 2025 - Apr 2026</p>
                   </div>
                 </div>
             </section>
